7d01661fae00be9ed2697cc7f347520ee34ff42e
Nate Verjinski <nverjins@gmail.com>
2026-08-20 18:05:58 -0500

    feat: created a home for ChartEngineLab default values (ChartMargins constant definitions)

---
+++ src/features/ChartEngineLab/panels/defaults.ts
@@ -0,0 +1,14 @@
+import { type ChartMargins } from "../../../chart-core/viewport/viewportTypes";
+export const PRICE_MARGINS: ChartMargins = {
+  top: 16,
+  right: 16,
+  bottom: 28,
+  left: 56,
+};
+
+export const VOLUME_MARGINS: ChartMargins = {
+  top: 8,
+  right: 16,
+  bottom: 24,
+  left: 56,
+};
